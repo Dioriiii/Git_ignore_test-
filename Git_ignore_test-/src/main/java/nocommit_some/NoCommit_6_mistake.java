1676f1a9999de95c80c37694f582c5ae58a93a12
chs048 <chs048@naver.com>
2023-10-25 11:38:13 +0900

    NoCommit_6_mistake.java 테스트

---
+++ Git_ignore_test-/src/main/java/nocommit_some/NoCommit_6_mistake.java
@@ -0,0 +1,10 @@
+package nocommit_some;
+
+public class NoCommit_6_mistake {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("~NoCommit_6_mistake.java 테스트 파일임둥~");
+		
+	}
+}
